c6ebb720254a58ec773a0c5f802e2718ce902e0e
Alex Perez <aperezm85@gmail.com>
2024-03-30 09:11:13 +0000

    Spotify clone using Astro

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,43 @@
+import { Pause, Play } from "./Player";
+import { usePlayerStore } from "@/store/playerStore";
+
+export function CardPlayButton({ id, size = "small" }) {
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
+    usePlayerStore((state) => state);
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist?.id === id;
+
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false);
+      return;
+    }
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { songs, playlist } = data;
+
+        setIsPlaying(true);
+        setCurrentMusic({
+          songs,
+          playlist,
+          song: songs[0],
+        });
+      });
+  };
+
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
+
+  return (
+    <button
+      onClick={handleClick}
+      className="card-plat-button rounded-full bg-green-500 hover:scale-[1.02] transition hover:bg-green-400 p-4"
+    >
+      {isPlayingPlaylist ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
+    </button>
+  );
+}
