7a904ff3b77e65733b1594267436bab2e1d9a38e
Alex Perez <aperezm85@gmail.com>
2024-03-30 17:21:51 +0000

    Added configuration and node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aperezm85.github.io',
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
