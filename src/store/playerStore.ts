22b602f36883d85fcdaaaa4c22fec4c539ea8a34
Alex Perez <aperezm85@gmail.com>
2024-03-30 12:07:45 +0000

    Converted Store to Typescript

---
+++ src/store/playerStore.ts
@@ -0,0 +1,30 @@
+import type { Playlist, Song } from "@/lib/data";
+import { create } from "zustand";
+
+export interface CurrentMusic {
+  playlist: Playlist | null;
+  song: Song | null;
+  songs: Song[];
+}
+
+export interface PlayerStore {
+  isPlaying: boolean;
+  currentMusic: CurrentMusic;
+  volume: number;
+  setVolume: (volume: number) => void;
+  setIsPlaying: (isPlaying: boolean) => void;
+  setCurrentMusic: (currentMusic: CurrentMusic) => void;
+}
+
+export const usePlayerStore = create<PlayerStore>((set) => ({
+  isPlaying: false,
+  currentMusic: {
+    playlist: null,
+    song: null,
+    songs: [],
+  },
+  volume: 1,
+  setVolume: (volume) => set({ volume }),
+  setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setCurrentMusic: (currentMusic) => set({ currentMusic }),
+}));
